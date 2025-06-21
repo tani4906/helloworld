e1e35d6d05103d44b208561bef46f2c55baed622
tanishamac <tanishamac@tanishamacs-MacBook-Air.local>
2025-06-21 22:27:16 +0530

    created sidebar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import { assets } from './assets/assets.js';
+const Spotifyimage = () => {
+  return(
+  <div style={{
+    display:'flex',
+    justifyContent:'center',
+    alignItems:'center',
+    gap:'10px',
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+  }}>
+    <img src={assets.spotify_logo} alt="spotifylogo"
+    style={{
+      display: 'block',
+      height:'50px',
+      width:'50px',
+      
+    }}/>
+    <h1>Spotify</h1>
+  </div>
+  );
+}
+const Home = () => {
+  return(
+    <div className="home-button">
+      <img src={assets.home_icon} alt="home"/>
+      <span>Home</span>
     </div>
   );
 }
+const Search = () => {
+  return(
+    <div className="home-button">
+      <img src={assets.search_icon} alt="search"/>
+      <span>Search</span>
+    </div>
+  );
+}
+const Stack = () => {
+  return(
+    <div className="home-button">
+      <img src={assets.stack_icon} alt="stack"/>
+      <span>Your library</span>
+    </div>
+  );
+}
+const Myapp = () => {
+  return (
+    <div className="flex-container">
+      <div className="box box1">
+         <Spotifyimage/> 
+         <Home/>
+         <Search/>
+         <Stack/>
+      </div>
+      <div className="box box2">Box 2</div>
+    </div>
+  );
+}
+export default Myapp;
 
-export default App;
+
+
